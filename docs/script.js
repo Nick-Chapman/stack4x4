4765fe6579ddd3bc3677e196d31c7303eff1e1e4
Nick Chapman <nic@amadido.co.uk>
2020-02-22 17:37:32 +0000

    refactor to avoid global state

--- docs/script.js
+++ docs/script.js
@@ -7,30 +7,29 @@ const green = 'rgb(83,225,56)'
 
 const size = 8
 
-let board = []
-let lastCell = '??'
-let nextPlayer = 1
-let hover
-
 init()
-reset()
 
 function init() {
-
+    const board = []
+    const hover = undefined
+    const lastPos = undefined
+    const nextPlayer = 1
+    s = { board, hover, lastPos, nextPlayer }
+    document.getElementById('reset').onclick = reset(s)
     for(let i = 0; i < size ; i++) {
         for(let j = 0; j < size; j++) {
+            let pos = [i,j]
             const canvas = document.createElement('canvas')
             canvas.width = 50
             canvas.height = 50
             canvas.setAttribute('class','cell')
-            canvas.onclick = onClick(i,j)
-            canvas.onmouseover = onMouseOver(i,j)
-            canvas.onmouseout = onMouseOut(i,j)
+            canvas.onclick = click(s,pos)
+            canvas.onmouseover = mouseOver(s,pos)
+            canvas.onmouseout = mouseOut(s,pos)
             document.body.appendChild(canvas)
             cell = { }
             cell.canvas = canvas
             cell.player = 0;
-            cell.name = cellName(i,j)
             board[i*size+j] = cell
             cell.ctx = canvas.getContext('2d')
         }
@@ -38,21 +37,7 @@ function init() {
         lineBreak.setAttribute('class','break')
         document.body.appendChild(lineBreak)
     }
-    document.getElementById('reset').onclick = reset
-}
-
-function onMouseOver(i,j) { return function(e) { hover = [i,j]; redraw()} }
-function onMouseOut(i,j) { return function(e) { hover = undefined; redraw()} }
-
-function reset() {
-    for(let i = 0; i < size ; i++) {
-        for(let j = 0; j < size; j++) {
-            let cell = board[i*size+j]
-            cell.player = 0
-        }
-    }
-    hover = undefined
-    redraw()
+    redraw(s)
 }
 
 function drawPiece(ctx,color) {
@@ -62,11 +47,21 @@ function drawPiece(ctx,color) {
     ctx.fill()
 }
 
-function redraw() {
-    document.getElementById('last').textContent = lastCell
+function drawStroke(ctx,color) {
+    ctx.beginPath()
+    ctx.lineWidth = 4
+    ctx.strokeStyle = color
+    ctx.arc (25, 25, 18, 0, 2 * Math.PI)
+    ctx.stroke()
+}
+
+function redraw(s) {
+    const {board,lastPos,hover,nextPlayer} = s
+    const lastCellName = lastPos ? cellName(lastPos) : '??'
+    document.getElementById('last').textContent = lastCellName
     for(let i = 0; i < size ; i++) {
         for(let j = 0; j < size; j++) {
-            let cell = board[i*size+j]
+            const cell = board[i*size+j]
             const canvas = cell.canvas
             const ctx = cell.ctx
             ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -77,69 +72,92 @@ function redraw() {
             }
             if (cell.player === 0) {
                 if (hover) {
-                    if (isLegalMove(hover)) {
-                        let [hi,hj] = hover
+                    if (isLegalMove(s,hover)) {
+                        const [hi,hj] = hover
                         if (hi === i && hj === j) {
-                            drawPiece(ctx,white)
+                            drawStroke(ctx,colourOfPlayer(s.nextPlayer))
                         }
                     }
                 }
-            }
-            if (cell.player === 1) {
-                drawPiece(ctx,purple)
-            }
-            if (cell.player === 2) {
-                drawPiece(ctx,green)
+            } else {
+                drawPiece(ctx,colourOfPlayer(cell.player))
             }
         }
     }
 }
 
-function cellName(i,j) {
-    return String.fromCharCode(65 + j) + String(size-i)
+function colourOfPlayer(player) {
+    if (player === 1) return purple
+    if (player === 2) return green
+    alert("colourOfPlayer!")
 }
 
-function onClick(i,j) { return function(e) {
-    //let cell = board[i*size+j]
-    let pos = [i,j]
-    if (isLegalMove(pos)) {
-        let cell = getCell(pos)
-        cell.player = nextPlayer
-        lastCell = cellName(i,j)
-        nextPlayer = 3-nextPlayer;
-        redraw()
+function reset(s) { return function() {
+    s.lastPos = undefined
+    s.hover = undefined
+    for(let i = 0; i < size ; i++) {
+        for(let j = 0; j < size; j++) {
+            const pos = [i,j]
+            cellAt(s,pos).player = 0
+        }
+    }
+    redraw(s)
+}}
+
+function mouseOver(s,pos) { return function(e) {
+    s.hover = pos
+    redraw(s)
+}}
+
+function mouseOut(s,pos) { return function(e) {
+    s.hover = undefined
+    redraw(s)
+}}
+
+function click(s,pos) { return function(e) {
+    if (isLegalMove(s,pos)) {
+        cellAt(s,pos).player = s.nextPlayer
+        s.lastPos = pos
+        s.nextPlayer = 3 - s.nextPlayer;
+        redraw(s)
     }
 }}
 
-function isLegalMove(pos) {
-    return empty(pos) && supported(pos)
+
+function isLegalMove(s,pos) {
+    return empty(s,pos) && supported(s,pos)
 }
 
-function empty(pos) {
-    return playerAt(pos) === 0
+function empty(s,pos) {
+    return playerAt(s,pos) === 0
 }
 
-function supported(pos) {
-    let [i,j] = pos
-    return (pillarX(0,i-1,j) || pillarX(i+1,size-1,j) ||
-            pillarY(i,0,j-1) || pillarY(i,j+1,size-1))
+function supported(s,pos) {
+    const [i,j] = pos
+    return (pillarX(s,0,i-1,j) || pillarX(s,i+1,size-1,j) ||
+            pillarY(s,i,0,j-1) || pillarY(s,i,j+1,size-1))
 }
 
-function pillarX(x1,x2,y) {
+function pillarX(s,x1,x2,y) {
     if (x1 > x2) return true
-    else return !empty([x1,y]) && pillarX(x1+1,x2,y)
+    else return !empty(s,[x1,y]) && pillarX(s,x1+1,x2,y)
 }
 
-function pillarY(x,y1,y2) {
+function pillarY(s,x,y1,y2) {
     if (y1 > y2) return true
-    else return !empty([x,y1]) && pillarY(x,y1+1,y2)
+    else return !empty(s,[x,y1]) && pillarY(s,x,y1+1,y2)
 }
 
-function playerAt(pos) {
-    return getCell(pos).player
+function playerAt(s,pos) {
+    return cellAt(s,pos).player
 }
 
-function getCell(pos) {
-    let [i,j] = pos
-    return board[i*size+j]
+function cellAt(s,pos) {
+    const [i,j] = pos
+    return s.board[i*size+j]
+}
+
+function cellName(pos) {
+    const [i,j] = pos
+    return String.fromCharCode(65 + j) + String(size-i)
 }
